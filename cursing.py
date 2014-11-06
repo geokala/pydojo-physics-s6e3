e52910bc34d27ec822ef66746deec444d93be2ca
Tim Golden <mail@timgolden.me.uk>
2014-11-06 21:44:55 +0000

    Show on screen

---
+++ cursing.py
@@ -0,0 +1,62 @@
+#!python3
+import os, sys
+import time
+
+import curses
+
+import lookupondarkness
+
+def log(text):
+    with open("cursing.log", "a") as f:
+        f.write("%s\n" % text)
+
+INTERVAL = 0.1
+
+type_map = {
+  "smoke" : ".",
+  "fire" : "/",
+}
+
+def frames():
+    for i in range(10):
+        for j in range(10):
+            for type in "fog", "fire", "water":
+                yield [(i, j, type), (i * 2, j * 2, type), (i * 3, j * 3, type)]
+
+def get_objects(generator, screen):
+    maxy, maxx = screen.getmaxyx()
+    for frame in generator(maxy, maxx):
+        log(frame)
+        screen.clear()
+        for (x, y), object_type in frame:
+            log("x, y, object_type: %s, %s, %s" % (x, y, object_type))
+            place_on_screen(screen, x, y, object_type)
+        screen.refresh()
+        time.sleep(INTERVAL)
+
+def place_on_screen(screen, x, y, object_type):
+    x, y = translate(screen, x, y)
+    log("x, y: %s, %s" % (x, y))
+    screen.addch(y, x, type_map[object_type])
+
+def translate(screen, x, y):
+    """Translate the coordinate system from the one from the Physics engine
+
+    x - The X Coordinate
+    y - The Y Coordinate
+    """
+    #
+    # For now, do nothing
+    #
+    maxy, maxx = screen.getmaxyx()
+    log("maxx, maxy: %s, %s" % (maxx, maxy))
+    x, y = int(x), int(maxy - y)
+    if not(0 <= x <= maxx and 0 <= y <= maxy):
+        raise RuntimeError("%s, %s not a valid coordinate within %s, %s" % (x, y, maxx, maxy))
+    return x, y
+
+def main(stdscr):
+    get_objects(lookupondarkness.run, stdscr)
+
+if __name__ == '__main__':
+    curses.wrapper(main)
