a4520e1c4ec2e6181e27f7e121f4b20c0466cf62
Madda <nobody.youknow@local>
2014-11-06 21:13:12 +0000

    And he created gravity, and it was... good-ish

--- lookupondarkness.py
+++ lookupondarkness.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import time
 
 class Particle:
     def __init__(self, gravity, particle_type, vel=[0.0], pos=[0,0]):
@@ -9,9 +10,23 @@ class Particle:
 
     def move(self):
         self.vel[1] = self.vel[1] - self.gravity
+        self.pos = [self.pos[axis] + self.vel[axis] for axis in (0,1)]
+        return self.get()
 
     def get(self):
         return self.pos, self.type
 
 
-        
+def run(width=100):
+
+    particles = []
+    for i in range(0, 5):
+        particles.append(Particle(10, "smoke", [0, 100], [width/2, 0]))
+
+    while True:
+        yield [p.move() for p in particles]
+
+if __name__ == "__main__":
+    for item in run():
+        print(item)
+        time.sleep(1)
